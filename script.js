69cae5ce5ccc6d01e8df56e5d9b6ab8070b09895
Laura Gularte <laurabgularte@gmail.com>
2026-03-02 13:49:18 -0300

    initial login page

---
+++ script.js
@@ -0,0 +1,8 @@
+document.getElementById('loginForm').addEventListener('submit', function(e) {
+    e.preventDefault();
+
+    const email = document.getElementById('email').value;
+    const password = document.getElementById('password').value;
+
+    console.log('Dados capturados:', { email, password });
+});
